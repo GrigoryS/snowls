73c4d13c4fc9da1ad1ae537b4fa2112ffe45819f
isUndefined <simon.smith.g@gmail.com>
2013-12-24 18:24:30 +0200

    Update HookSnow.class.php
    Added hooks TplBodyBegin, TplMenuSettings

--- classes/hooks/HookSnow.class.php
+++ classes/hooks/HookSnow.class.php
@@ -3,13 +3,31 @@
 class PluginSnow_HookSnow extends Hook {
 
 	public function RegisterHook () {
-		$this -> AddHook ('template_html_head_end', 'HeadEnd');                              // for main init
+		if (Config::Get('plugin.snow.show_snow')){
+			$this -> AddHook ('template_html_head_end', 'HeadEnd');                              // for main init
+		}
+		
+		$this -> AddHook ('template_menu_settings_settings_item', 'TplMenuSettings');        // 
+		
+		$this -> AddHook ('template_body_begin', 'TplBodyBegin');        // 
 	}
 	
 	// ---
 
 	public function HeadEnd () {
 		return $this -> Viewer_Fetch (Plugin::GetTemplatePath (__CLASS__) . 'snow_begin.tpl');
+	}
+	
+	// ---
+	
+	public function TplMenuSettings () {
+		return $this -> Viewer_Fetch (Plugin::GetTemplatePath (__CLASS__) . 'menu.snow.tpl');
+	}
+	
+	// ---
+	
+	public function TplBodyBegin () {
+		return $this -> Viewer_Fetch (Plugin::GetTemplatePath (__CLASS__) . 'inject.toys.tpl');
 	}
 	
 	// ---
