ffa468868d887de7222a92677c49326c323322b9
isUndefined <simon.smith.g@gmail.com>
2014-01-02 10:36:00 +0200

    Update dump.sql

--- dump.sql
+++ dump.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `prefix_snow_toys` (
   `toy_id` int(11) NOT NULL AUTO_INCREMENT,
   `toy_src` varchar(200) NOT NULL,
   PRIMARY KEY (`toy_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -16,13 +16,11 @@ CREATE TABLE IF NOT EXISTS `prefix_snow_toys` (
 -- Table structure for table `prefix_snow_users`
 --
 
-CREATE TABLE IF NOT EXISTS `prefix_snow_users` (
+CREATE TABLE IF NOT EXISTS `ls_snow_users` (
   `snow_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `toy_id` int(11) DEFAULT NULL,
-  `top` int(11) DEFAULT NULL,
-  `left` int(11) DEFAULT NULL,
+  `snow_extra` text,
   PRIMARY KEY (`snow_id`),
-  KEY `user_id` (`user_id`),
-  KEY `toy_id` (`toy_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  UNIQUE KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
